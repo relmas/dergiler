aec54041324b955dca376b7423c13605b9b4007a
relmas <arkeorelmas@gmail.com>
2015-10-10 13:21:00 +0300

    ekleme
    ekleme

---
+++ www/js/my-app.js
@@ -0,0 +1,35 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true,
+    
+});
+
+  // Init slider and store its instance in mySwiper variable
+  var mySwiper = myApp.swiper('.swiper-container', {
+    pagination:'.swiper-pagination',
+    paginationClickable: true,
+	        preloadImages: false,
+        // Enable lazy loading
+        lazyLoading: true,
+    loop: true
+  }); 
+  
+
+
+/*var tab=$('.tab');
+tab.find('.tabMenu .tabMenuList').click(function(){
+    var index=$(this);
+    $('.tabContent .tabContentList').eq(index).show().siblings().hide();
+})*/
+
+
+
+
+ 
